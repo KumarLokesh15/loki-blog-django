b1f80539d91804680290b6c75f952d7624d5eecf
LOKI <kumarlokesh15012000@gmail.com>
2022-03-07 12:25:24 +0530

    url changed

--- mySite/urls.py
+++ mySite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -24,8 +25,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("blog.urls")),
-    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] 
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
 # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
